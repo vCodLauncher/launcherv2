a92071ba58235a033cc23ca0ec850bfc288f5319
wysixontheflux <bitpoch@icloud.com>
2023-09-24 12:45:34 +0200

    page

--- src/templates/home/Home.jsx
+++ src/templates/home/Home.jsx
@@ -1,10 +1,11 @@
 // HOME PAGE
 
+import styles from './Home.module.css'
+import VCod from "../vcod/VCod";
+
 const Home = () => {
     return (
-        <>
-
-        </>
+        <VCod />
     )
 }
 
